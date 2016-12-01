33308b008df55c90d85c0be88eeae7d5d6f01fea
Loren Barrick <lorenbarrick@gmail.com>
2016-11-30 18:41:02 -0800

    board starting to form, need to apply css and include input for inner cells

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      gameBoard: null
+      gameBoard: null,
+      count: 0
     };
   }
 
@@ -23,16 +24,191 @@ class App extends React.Component {
     for (let i = 0; i < 10; i++) {
         arr[i] = [];
         for (let j = 0; j < 10; j++) {
-            arr[i][j] = 0;
+            arr[i][j] = {
+                          coordinateX: i,
+                          coordinateY: j,
+                          containsShip: false,
+                          alreadyAttacked: false
+                        };
         }
     }
     this.setState({ gameBoard: arr });
   }
 
+   verticalCount() {
+    this.setState({ count: this.state.count++ });
+   }
+
   render() {
     return (
-      <div>
-        Hello World!
+      <div
+        className='table'
+      >
+        <div className='row'>
+          <div
+            className='cell-outer'
+            style={{
+                float: 'left',
+                width: '30px',
+                padding: '2px',
+                height: '30px',
+                borderRight: '1px solid #5C9DC2',
+                borderBottom: '1px solid #5C9DC2'
+              }}
+          >
+          </div>
+          <div
+            className='cell-outer'
+            style={{
+                float: 'left',
+                width: '30px',
+                padding: '2px',
+                height: '30px',
+                borderRight: '1px solid #5C9DC2',
+                borderBottom: '1px solid #5C9DC2'
+              }}
+          >
+          A
+          </div>
+          <div
+            className='cell-outer'
+            style={{
+                float: 'left',
+                width: '30px',
+                padding: '2px',
+                height: '30px',
+                borderRight: '1px solid #5C9DC2',
+                borderBottom: '1px solid #5C9DC2'
+              }}
+          >
+          B
+          </div>
+          <div
+            className='cell-outer'
+            style={{
+                float: 'left',
+                width: '30px',
+                padding: '2px',
+                height: '30px',
+                borderRight: '1px solid #5C9DC2',
+                borderBottom: '1px solid #5C9DC2'
+              }}
+          >
+          C
+          </div>
+          <div
+            className='cell-outer'
+            style={{
+                float: 'left',
+                width: '30px',
+                padding: '2px',
+                height: '30px',
+                borderRight: '1px solid #5C9DC2',
+                borderBottom: '1px solid #5C9DC2'
+              }}
+          >
+          D
+          </div>
+          <div
+            className='cell-outer'
+            style={{
+                float: 'left',
+                width: '30px',
+                padding: '2px',
+                height: '30px',
+                borderRight: '1px solid #5C9DC2',
+                borderBottom: '1px solid #5C9DC2'
+              }}
+          >
+          E
+          </div>
+          <div
+            className='cell-outer'
+            style={{
+                float: 'left',
+                width: '30px',
+                padding: '2px',
+                height: '30px',
+                borderRight: '1px solid #5C9DC2',
+                borderBottom: '1px solid #5C9DC2'
+              }}
+          >
+          F
+          </div>
+          <div
+            className='cell-outer'
+            style={{
+                float: 'left',
+                width: '30px',
+                padding: '2px',
+                height: '30px',
+                borderRight: '1px solid #5C9DC2',
+                borderBottom: '1px solid #5C9DC2'
+              }}
+          >
+          G
+          </div>
+          <div
+            className='cell-outer'
+            style={{
+                float: 'left',
+                width: '30px',
+                padding: '2px',
+                height: '30px',
+                borderRight: '1px solid #5C9DC2',
+                borderBottom: '1px solid #5C9DC2'
+              }}
+          >
+          H
+          </div>
+          <div
+            className='cell-outer'
+            style={{
+                float: 'left',
+                width: '30px',
+                padding: '2px',
+                height: '30px',
+                borderRight: '1px solid #5C9DC2',
+                borderBottom: '1px solid #5C9DC2'
+              }}
+          >
+          I
+          </div>
+          <div
+            className='cell-outer'
+            style={{
+                float: 'left',
+                width: '30px',
+                padding: '2px',
+                height: '30px',
+                borderRight: '1px solid #5C9DC2',
+                borderBottom: '1px solid #5C9DC2'
+              }}
+          >
+          J
+          </div>
+        </div>
+        {this.state.gameBoard.map((gameBoardArray, index) => {
+          return (
+            <div className='row'>
+              <div className='cell-outer'>
+              {gameBoardArray.map((startingTile, index) =>
+                <div className={`${index}`}>
+                  {index === 0 ? <div>{this.state.count++}</div> : null}
+                </div>
+              )}
+              {gameBoardArray.map((tile, index) => {
+                return (
+
+                  <div className='cell-inner'>
+                    <div>Tile</div>
+                  </div>
+                );
+              })}
+              </div>
+            </div>
+          );
+        })}
       </div>
     );
   }
